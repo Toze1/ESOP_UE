334d9a050c0dc8532a2f68e11568a8b3826b7cfd
Toze1 <rush@chello.at>
2016-10-09 12:39:24 +0200

    Fixed for y and c

--- src/Aufgabe14.java
+++ src/Aufgabe14.java
@@ -12,21 +12,26 @@ public class Aufgabe14 {
         double b = sc.nextDouble();
         System.out.println("Geben Sie c ein: ");
         double c = sc.nextDouble();
+        System.out.println("Geben Sie y ein: ");
+        double y = sc.nextDouble();
 
         //To Solve y = ax² + bx + c // z.b d> 0; 4, 6, -4 ... x1 = 0.5, x2 = -2
         //To Solve y = ax² + bx + c // z.b d< 0; 5, 8, 9 ... Keine Lösung
         //To Solve y = ax² + bx + c // z.b.d=0; 1,-4,4 ... eine Lösung (2)
 
-        System.out.println("Die zu lösende Gleichung lautet:\n" + a + "x² "+"+ "+ b + "x "+"+ "+ c);
+        System.out.println("Die zu lösende Gleichung lautet:\n" + a + "x² "+"+ "+ b + "x "+"+ "+ c +"= "+y+" // Minus "+y);
+        c = c-y;
+        System.out.println("Die zu lösende Gleichung lautet:\n" + a + "x² "+"+ "+ b + "x "+"+ "+ c +"= "+"0");
+
 
         double d = ((b * b) - (4 * (a * c)));
 
         if (d>0)  {
-        double x1 = ((-b)+ Math.sqrt((b*b)-(4*a*c)))/(2*a);
-        double x2 = ((-b)- Math.sqrt((b*b)-(4*a*c)))/(2*a);
+            double x1 = ((-b)+ Math.sqrt((b*b)-(4*a*c)))/(2*a);
+            double x2 = ((-b)- Math.sqrt((b*b)-(4*a*c)))/(2*a);
 
-        System.out.println("x1 = " + x1);
-        System.out.println("x2 = " + x2); }
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2); }
 
         else if (d == 0) {
 
@@ -39,3 +44,4 @@ public class Aufgabe14 {
 
     }
 }
+
